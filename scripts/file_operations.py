900ec05cbb410d889efc56bdf2fe70c6e06efdc7
mcoolidge <mat@orbitalhealth.com>
2023-04-11 15:00:24 -0500

    more actions

--- scripts/file_operations.py
+++ scripts/file_operations.py
@@ -81,3 +81,83 @@ def search_files(directory):
             found_files.append(relative_path)
 
     return found_files
+
+def list_files(directory):
+    found_files = []
+
+    if directory == "" or directory == "/":
+        search_directory = working_directory
+    else:
+        search_directory = safe_join(working_directory, directory)
+
+    for root, _, files in os.walk(search_directory):
+        for file in files:
+            if file.startswith('.'):
+                continue
+            found_files.append(file)
+
+    return found_files
+
+def list_directories(directory):
+    found_directories = []
+
+    if directory == "" or directory == "/":
+        search_directory = working_directory
+    else:
+        search_directory = safe_join(working_directory, directory)
+
+    for root, directories, _ in os.walk(search_directory):
+        for directory in directories:
+            if directory.startswith('.'):
+                continue
+            found_directories.append(directory)
+
+    return found_directories
+
+def create_directory(directory):
+    try:
+        directory_path = safe_join(working_directory, directory)
+        os.makedirs(directory_path)
+        return "Directory created successfully."
+    except Exception as e:
+        return "Error: " + str(e)
+    
+def delete_directory(directory):
+    try:
+        directory_path = safe_join(working_directory, directory)
+        os.rmdir(directory_path)
+        return "Directory deleted successfully."
+    except Exception as e:
+        return "Error: " + str(e)
+    
+def get_current_directory():
+    return working_directory
+
+
+def rename_file(old_name, new_name):
+    try:
+        old_path = safe_join(working_directory, old_name)
+        new_path = safe_join(working_directory, new_name)
+        os.rename(old_path, new_path)
+        return "File renamed successfully."
+    except Exception as e:
+        return "Error: " + str(e)
+    
+def rename_directory(old_name, new_name):
+    try:
+        old_path = safe_join(working_directory, old_name)
+        new_path = safe_join(working_directory, new_name)
+        os.rename(old_path, new_path)
+        return "Directory renamed successfully."
+    except Exception as e:
+        return "Error: " + str(e)
+    
+def copy_file(old_name, new_name):
+    try:
+        old_path = safe_join(working_directory, old_name)
+        new_path = safe_join(working_directory, new_name)
+        os.copyfile(old_path, new_path)
+        return "File copied successfully."
+    except Exception as e:
+        return "Error: " + str(e)
+    
